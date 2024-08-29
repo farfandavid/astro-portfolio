49b1d92476f11b408a26e39d5477dafd52b6dc9a
farfandavid <39929763@fi.unju.edu.ar>
2024-08-29 16:15:14 -0300

    chore: Add sitemap integration to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://farfetchdev.web.app",
   output: "static",
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx(), sitemap()]
 });
