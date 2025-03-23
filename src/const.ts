806603c0dd3d5ce333d012b916a4f713af03cdc9
farfandavid <39929763@fi.unju.edu.ar>
2025-03-22 23:14:21 -0300

    fix url and added xintheria

--- src/const.ts
+++ src/const.ts
@@ -13,15 +13,15 @@ export const projectsData: ProjectData[] = [
         id: "snapmenu",
         title: "SnapMenu",
         description: "SnapMenu es una aplicación web que permite a los restaurantes publicar sus menús de forma digital y a los clientes acceder a ellos de forma rápida y sencilla.",
-        urlImage: "https://cdn.snapmenu.online/og.webp",
-        web: "https://www.snapmenu.online",
+        urlImage: "https://pub-73fb1a1714204ef38ed19ef2f83d89a9.r2.dev/og.webp",
+        web: "https://snapmenu.onrender.com",
         tags: ["#nodejs", "#astro", "#reactjs", "#html", "#js"]
     },
     {
         id: "aicvanalizer",
         title: "AI CV Analizer",
         description: "AI CV Analizer es una aplicación web que permite analizar los CVs con la IA de Gemini y obtener una puntuacion.",
-        urlImage: "https://cdn.snapmenu.online/103_1x_shots_so.png",
+        urlImage: "https://pub-73fb1a1714204ef38ed19ef2f83d89a9.r2.dev/103_1x_shots_so.png",
         tags: ["#astro", "#nodejs", "#gemini", "#html", "#js"],
         web: "https://www.aicvanalizer.online",
         github: "https://github.com/farfandavid/aicvanalizer"
@@ -30,9 +30,17 @@ export const projectsData: ProjectData[] = [
         id: "ai_gemini_html_generator",
         title: "AI Gemini HTML Generator",
         description: "AI Gemini HTML Generator es una aplicación web que permite generar el HTML con la IA de Gemini.",
-        urlImage: "https://cdn.snapmenu.online/aihtmlgenerator.jpeg",
+        urlImage: "https://pub-73fb1a1714204ef38ed19ef2f83d89a9.r2.dev/aihtmlgenerator.jpeg",
         tags: ["#astro", "#nodejs", "#gemini", "#html", "#js"],
         github: "https://github.com/farfandavid/ai_gemini_html_generator"
+    },
+    {
+        id: "xintheria_ai",
+        title: "Xintheria AI",
+        description: "Xintheria AI es una aplicación web que permite a los usuarios interactuar con la IA de Xintheria.",
+        urlImage: "https://pub-73fb1a1714204ef38ed19ef2f83d89a9.r2.dev/xintheria-en.jpg",
+        tags: ["#astro", "#nodejs", "#xintheria", "#html", "#js"],
+        web: "https://www.xintheria.com",
     }
     // Puedes añadir más objetos de proyecto aquí
 ];
