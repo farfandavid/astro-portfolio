36c8964fcd8d81249ffcde016ecd0d6424f42d3f
farfandavid <39929763@fi.unju.edu.ar>
2025-05-27 02:58:18 -0300

    refactor: mejorar la estructura de datos de proyectos y ajustar el componente CardProject

--- src/const.ts
+++ src/const.ts
@@ -1,46 +1,43 @@
-
 interface ProjectData {
-    id: string;
-    title: string;
-    description: string;
-    tags: string[];
-    github?: string;
-    web?: string;
-    urlImage: string;
+  id: string;
+  title: string;
+  description: string;
+  tags: string[];
+  github?: string;
+  web?: string;
+  urlImage: string;
 }
 export const projectsData: ProjectData[] = [
-    {
-        id: "snapmenu",
-        title: "SnapMenu",
-        description: "SnapMenu es una aplicación web que permite a los restaurantes publicar sus menús de forma digital y a los clientes acceder a ellos de forma rápida y sencilla.",
-        urlImage: "https://pub-73fb1a1714204ef38ed19ef2f83d89a9.r2.dev/og.webp",
-        web: "https://snapmenu.onrender.com",
-        tags: ["#nodejs", "#astro", "#reactjs", "#html", "#js"]
-    },
-    {
-        id: "aicvanalizer",
-        title: "AI CV Analizer",
-        description: "AI CV Analizer es una aplicación web que permite analizar los CVs con la IA de Gemini y obtener una puntuacion.",
-        urlImage: "https://pub-73fb1a1714204ef38ed19ef2f83d89a9.r2.dev/103_1x_shots_so.png",
-        tags: ["#astro", "#nodejs", "#gemini", "#html", "#js"],
-        web: "https://www.aicvanalizer.online",
-        github: "https://github.com/farfandavid/aicvanalizer"
-    },
-    {
-        id: "ai_gemini_html_generator",
-        title: "AI Gemini HTML Generator",
-        description: "AI Gemini HTML Generator es una aplicación web que permite generar el HTML con la IA de Gemini.",
-        urlImage: "https://pub-73fb1a1714204ef38ed19ef2f83d89a9.r2.dev/aihtmlgenerator.jpeg",
-        tags: ["#astro", "#nodejs", "#gemini", "#html", "#js"],
-        github: "https://github.com/farfandavid/ai_gemini_html_generator"
-    },
-    {
-        id: "xintheria_ai",
-        title: "Xintheria AI",
-        description: "Xintheria AI es una aplicación web que permite a los usuarios interactuar con la IA de Xintheria.",
-        urlImage: "https://pub-73fb1a1714204ef38ed19ef2f83d89a9.r2.dev/xintheria-en.jpg",
-        tags: ["#astro", "#nodejs", "gemini", "deepseek", "#react", "#html", "#js"],
-        web: "https://www.xintheria.com",
-    }
-    // Puedes añadir más objetos de proyecto aquí
+  {
+    id: "aicvanalizer",
+    title: "AI CV Analizer",
+    description:
+      "AI CV Analizer es una aplicación web que permite analizar los CVs con la IA de Gemini y obtener una puntuacion.",
+    urlImage:
+      "https://pub-73fb1a1714204ef38ed19ef2f83d89a9.r2.dev/103_1x_shots_so.png",
+    tags: ["#astro", "#nodejs", "#gemini", "#html", "#js"],
+    github: "https://github.com/farfandavid/aicvanalizer",
+  },
+  {
+    id: "ai_gemini_html_generator",
+    title: "AI Gemini HTML Generator",
+    description:
+      "AI Gemini HTML Generator es una aplicación web que permite generar el HTML con la IA de Gemini.",
+    urlImage:
+      "https://pub-73fb1a1714204ef38ed19ef2f83d89a9.r2.dev/aihtmlgenerator.jpeg",
+    tags: ["#astro", "#nodejs", "#gemini", "#html", "#js"],
+    github: "https://github.com/farfandavid/ai_gemini_html_generator",
+  },
+  {
+    id: "xintheria_ai",
+    title: "Xintheria Hackathon Clerk",
+    description:
+      "Xintheria AI es una aplicación web que permite Crear un sition web utilizando la IA de Gemini",
+    urlImage:
+      "https://pub-73fb1a1714204ef38ed19ef2f83d89a9.r2.dev/xintheria-en.jpg",
+    tags: ["#astro", "#nodejs", "gemini", "deepseek", "#react", "#html", "#js"],
+    web: "https://xintheria-hackathon-clerk.onrender.com/",
+    github: "https://github.com/farfandavid/xintheria-hackathon-clerk",
+  },
+  // Puedes añadir más objetos de proyecto aquí
 ];
